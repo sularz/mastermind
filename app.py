c70a039328a310a9f8f2b2f49df2c5fe8f0b0310
sularz <karolasularz@gmail.com>
2020-02-11 23:13:04 +0100

    tududutududu

--- app.py
+++ app.py
@@ -36,45 +36,44 @@ def game_ready():
     guess = []
     guess = request.form.get('answer')
 
-    if guess is None:
-        form = AnswerForm()
-        if form.validate_on_submit():
-            guess = list(form.answer.data)
+    form = AnswerForm()
+    if form.validate_on_submit():
+        guess = list(form.answer.data)
              
-            global game
-            while game:
+        global game
+        while game:
         
-                global attempts 
-                attempts += 1
+            global attempts 
+            attempts += 1
 
-                if attempts > 10:
-                    game = False
-                #print('Koniec gry')
+            if attempts > 10:
+                game = False
+                                #print('Koniec gry')
 
-                answer = []
-                global joinedlist
-                joinedlist = list(map(str, joinedlist)) #konwertuję do stringów, potrzebne przy sprawdzaniu cyfr
+            answer = []
+            global joinedlist
+            joinedlist = list(map(str, joinedlist)) #konwertuję do stringów, potrzebne przy sprawdzaniu cyfr
 
                             #sprawdzenie kolorów w odpowiedzi
-                for i in range(0, 7, 2):
-                    if guess[i] == joinedlist[i]: #poprawny kolor i prawidłowe miejsce
-                        answer.append("O")
-                    elif guess[i] in joinedlist:	
+            for i in range(0, 7, 2):
+                if guess[i] == joinedlist[i]: #poprawny kolor i prawidłowe miejsce
+                    answer.append("O")
+                elif guess[i] in joinedlist:	
                             #poprawny kolor, ale na złym miejscu
-                        answer.append("V")
-                    else: #zły kolor
-                        answer.append("X")
+                    answer.append("V")
+                else: #zły kolor
+                    answer.append("X")
                                 
                             #sprawdzenie cyfr w odpowiedzi
-                for i in range(1, 8, 2):
-                    if guess[i] == joinedlist[i]: #poprawna cyfra i prawidłowe miejsce
-                        answer.append("O")
-                    elif guess[i] in joinedlist:	
+            for i in range(1, 8, 2):
+                if guess[i] == joinedlist[i]: #poprawna cyfra i prawidłowe miejsce
+                    answer.append("O")
+                elif guess[i] in joinedlist:	
                             #poprawna cyfra, ale na złym miejscu
-                        answer.append("V")
-                    else: #zła cyfra
-                        answer.append("X")   
-        return render_template('game.html', title='Game', form=form, guess=guess)
+                    answer.append("V")
+                else: #zła cyfra
+                    answer.append("X")   
+    return render_template('game.html', title='Game', form=form, guess=guess, joinedlist=joinedlist)
 
 if __name__ == '__main__':
     app.run(debug=True)
