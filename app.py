10265fd5faf02ddcef73c5e92cb6415914a8d000
sularz <48519094+sularz@users.noreply.github.com>
2020-02-18 14:59:27 +0100

    działa

--- app.py
+++ app.py
@@ -26,47 +26,58 @@ def game_ready():
 
     guess = []
     answer_list = []
+    failure = True
     guess = request.form.get('answer')
     form = AnswerForm()
     
     if form.validate_on_submit():
         guess = form.answer.data
         guess = guess.split()
-        
-        global joinedlist
 
-        #sprawdzenie kolorów w odpowiedzi
-        for i in range(4):
-            if guess[i] == joinedlist[i]: #poprawny kolor i prawidłowe miejsce
-                answer_list.append("O")
-            elif guess[i] in joinedlist:	
-                #poprawny kolor, ale na złym miejscu
-                answer_list.append("V")
-            else: #zły kolor
-                answer_list.append("X")
+        global colors
+        #walidacja
+        if len(guess) == 4:
+            if guess[0] in colors and guess[1] in colors and guess[2] in colors and guess[3] in colors:
+                    failure=False
         
-        global all_answers
-        
-        if len(all_answers) < 10:
-            all_answers.append([guess, answer_list])
+        if failure == False:
+            global joinedlist
 
-            if guess == joinedlist:
-                winner = True
+            #sprawdzenie kolorów w odpowiedzi
+            for i in range(4):
+                if guess[i] == joinedlist[i]: #poprawny kolor i prawidłowe miejsce
+                    answer_list.append("O")
+                elif guess[i] in joinedlist:	
+                    #poprawny kolor, ale na złym miejscu
+                    answer_list.append("V")
+                else: #zły kolor
+                    answer_list.append("X")
+            
+            global all_answers
+            
+            if len(all_answers) < 10:
+                all_answers.append([guess, answer_list])
+
+                if guess == joinedlist:
+                    winner = True
+                    all_answers = guess = joinedlist = answer_list = []
+                    colors = ["czerwony", "czarny", "zielony", "niebieski"]
+                    color = random.choices(colors, k=4) #losowe kolory
+                    joinedlist = color
+                    return render_template('winnerloser.html', title='WinnerLoser', winner=winner, form=form)
+
+            if len(all_answers) == 10:
+                winner = False
                 all_answers = guess = joinedlist = answer_list = []
                 colors = ["czerwony", "czarny", "zielony", "niebieski"]
                 color = random.choices(colors, k=4) #losowe kolory
                 joinedlist = color
                 return render_template('winnerloser.html', title='WinnerLoser', winner=winner, form=form)
 
-        if len(all_answers) == 10:
-            winner = False
-            all_answers = guess = joinedlist = answer_list = []
-            colors = ["czerwony", "czarny", "zielony", "niebieski"]
-            color = random.choices(colors, k=4) #losowe kolory
-            joinedlist = color
-            return render_template('winnerloser.html', title='WinnerLoser', winner=winner, form=form)
+            failure=True
         
     return render_template('game.html', title='Game', form=form, guess=guess, answer_list=answer_list, joinedlist=joinedlist, all_answers=all_answers)
     
 if __name__ == '__main__':
     app.run(debug=True)   
+    #app.run(host = "0.0.0.0", port = 5078, debug = "True")
--- form.py
+++ form.py
@@ -3,5 +3,5 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 class AnswerForm(FlaskForm):
-    answer = StringField('answer', validators=[DataRequired()])
-    submit = SubmitField('submit')
+    answer = StringField('Tutaj wpisz swój kod, hakerze! Wpisuj dozwolone kolory: niebieski, zielony, czarny, czerwony!', validators=[DataRequired()])
+    submit = SubmitField('wyślij')
